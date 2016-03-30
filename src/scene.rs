5027933719921b7f45920036b8a112fde19dbc90
Drew Chandler <andrewschandler@gmail.com>
2016-03-30 11:18:33 -0700

    Make an Intersection type

--- src/scene.rs
+++ src/scene.rs
@@ -1,34 +1,65 @@
 use super::color::Color;
 use super::object::Object;
 use super::ray::Ray;
+use std::cmp::Ordering;
 
 pub struct Scene {
     pub background: Color,
     pub objects: Vec<Object>
 }
 
+pub struct Intersection<'a> {
+    pub object: &'a Object,
+    pub distance: f64
+}
+
+impl<'a> Intersection<'a> {
+    pub fn new(object: &'a Object, distance: f64) -> Intersection<'a> {
+        Intersection {
+            object: object,
+            distance: distance
+        }
+    }
+}
+
+#[derive(PartialEq,PartialOrd)]
+struct NonNan(f64);
+
+impl NonNan {
+    fn new(val: f64) -> Option<NonNan> {
+        if val.is_nan() {
+            None
+        } else {
+            Some(NonNan(val))
+        }
+    }
+}
+
+impl Eq for NonNan {}
+
+impl Ord for NonNan {
+    fn cmp(&self, other: &NonNan) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
 impl Scene {
     pub fn cast(&self, r: &Ray) -> Color {
         match self.nearest_interection(&r) {
-            Some((o, _)) => o.color.clone(),
+            Some(i) => i.object.color.clone(),
             _ => self.background.clone()
         }
     }
 
-    fn nearest_interection(&self, r: &Ray) -> Option<(&Object, f64)> {
+    fn nearest_interection(&self, r: &Ray) -> Option<Intersection> {
         self.objects.iter()
             .filter_map(|o| {
                 match o.intersect(&r) {
-                    Some(t) => Some((o, t)),
+                    Some(t) => Some(Intersection::new(o, t)),
                     _ => None
                 }
             })
-            .fold(None, |nearest, current| {
-                match nearest {
-                    Some((_, d)) if d < current.1 => nearest,
-                    _ => Some(current)
-                }
-            })
+            .min_by_key(|o| NonNan::new(o.distance))
     }
 }
 
